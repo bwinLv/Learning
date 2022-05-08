b7dec71be52d77ffd5ab243c3d18b5d5daa5b550
unknown <1379761953@qq.com>
2022-05-08 17:11:12 +0800

    初次提交代码

---
+++ compare.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+void compare(void)
+{
+    int x, y, z, t;
+    scanf("%d %d %d", &x, &y, &z);
+    if (x > y) {
+        t = x; x = y; y = t;
+    }
+    if (x > z) {
+        t = z; z = x; x = t;
+    }
+    if (y > z) {
+        t = y; y = z; z = t;
+    }
+    printf("small to big: %d %d %d\n", x, y, z);
+    printf("在公司开发了另一半的代码");
+    printf("在家开发的功能");
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello World\r\n");
+    system("pause");
+}
